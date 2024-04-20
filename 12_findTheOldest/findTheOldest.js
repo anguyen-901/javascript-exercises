ecf46ac6da161d95868801ebe92a42ded91c5f3a
Albert Nguyen <albertnguyen34@gmail.com>
2024-04-20 00:26:57 -0500

    solved 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    return peopleArray.reduce((oldest, current) => {
+        let oldestAge;
+        let currentAge;
+        if (current.yearOfDeath) {
+            currentAge = current.yearOfDeath - current.yearOfBirth;
+        } else {
+            let currentYear = new Date().getFullYear();
+            currentAge = currentYear - current.yearOfBirth;
+        }
 
+        if (oldest.yearOfDeath) {
+            oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        } else {
+            let currentYear = new Date().getFullYear();
+            oldestAge = currentYear - oldest.yearOfBirth;
+        }
+
+        if (currentAge > oldestAge) {
+            return current;
+        } else {
+            return oldest;
+        }
+    });
 };
 
 // Do not edit below this line
