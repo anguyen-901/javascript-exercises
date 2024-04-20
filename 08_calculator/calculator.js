13b89078673440b823862d20e0b0fcd9fc140310
Albert Nguyen <albertnguyen34@gmail.com>
2024-04-19 23:04:14 -0500

    solved calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+	return sumArray.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(multiplyArray) {
+  return multiplyArray.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (0 === a) {
+    return 1;
+  }
+
+  let result = a;
+
+  for (let index = a - 1; index > 0; index--) {
+    result = result * index;
+  }
+  return result;
 };
 
 // Do not edit below this line
