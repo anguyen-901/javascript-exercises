03399e31d214d46363e8e0747e47f44c61fef28f
Albert Nguyen <albertnguyen34@gmail.com>
2024-04-19 23:24:44 -0500

    solved 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    const regex = /[\s\p{P}]/gu;
+    let filteredText = a.replace(regex, '').toUpperCase();
+    let reverseFilteredText = filteredText.split('').reverse().join('');
+    return filteredText == reverseFilteredText;
 };
 
 // Do not edit below this line
